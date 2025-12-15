32b3de3bb5d3b49b882807618007bf05a56a20aa
Dritan Balisha <109618675+DritanBalisha@users.noreply.github.com>
2025-12-15 21:51:39 +0100

    Update Singup.tsx

--- src/pages/Singup.tsx
+++ src/pages/Singup.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link , useNavigate} from "react-router-dom";
+import API_URL from "../api/config.ts";
 
 export default function Signup() {
   const [name, setName] = useState("");
@@ -11,7 +12,7 @@ export default function Signup() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await fetch(`${process.env.REACT_APP_API_URL}/signup', {
+      const res = await fetch(`${API_URL}/signup', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
